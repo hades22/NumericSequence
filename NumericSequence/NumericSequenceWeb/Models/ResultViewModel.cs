946d3420e84bb82cce68f422c78a8d7ed6a1dbf4
Hades <hades22.sun@gmail.com>
2017-07-28 18:50:02 +1000

    improve view

--- NumericSequence/NumericSequenceWeb/Models/ResultViewModel.cs
+++ NumericSequence/NumericSequenceWeb/Models/ResultViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace NumericSequenceWeb.Models
@@ -18,5 +19,71 @@ namespace NumericSequenceWeb.Models
         public List<int> Even { get; set; }
         public ArrayList Multipler { get; set; }
         public ArrayList Fibonacci { get; set; }
+
+        public string AllList
+        {
+            get
+            {
+                StringBuilder list = new StringBuilder();
+                foreach(var value in All)
+                {
+                    list.Append(value.ToString() + ", ");
+                }
+                return list.ToString();
+            }
+        }
+
+        public string OddList
+        {
+            get
+            {
+                StringBuilder list = new StringBuilder();
+                foreach (var value in Odd)
+                {
+                    list.Append(value.ToString() + ", ");
+                }
+                return list.ToString();
+            }
+        }
+
+        public string EvenList
+        {
+            get
+            {
+                StringBuilder list = new StringBuilder();
+                foreach (var value in Even)
+                {
+                    list.Append(value.ToString() + ", ");
+                }
+                return list.ToString();
+            }
+        }
+
+
+        public string MultiplerList
+        {
+            get
+            {
+                StringBuilder list = new StringBuilder();
+                foreach (var value in Multipler)
+                {
+                    list.Append(value.ToString() + ", ");
+                }
+                return list.ToString();
+            }
+        }
+
+        public string FibonacciList
+        {
+            get
+            {
+                StringBuilder list = new StringBuilder();
+                foreach (var value in Fibonacci)
+                {
+                    list.Append(value.ToString() + ", ");
+                }
+                return list.ToString();
+            }
+        }
     }
 }
