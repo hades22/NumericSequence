237f48c4bcfb8fd97ba68e28a37c913080888d9c
Hades <hades22.sun@gmail.com>
2017-07-28 20:04:08 +1000

    input validation

--- NumericSequence/NumericSequenceWeb/App_Start/RouteConfig.cs
+++ NumericSequence/NumericSequenceWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NumericSequenceWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Result", id = UrlParameter.Optional }
             );
         }
     }
